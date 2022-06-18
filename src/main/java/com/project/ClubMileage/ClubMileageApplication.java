d2b5fa4d7e621e71c65411c12665610b1c6103cb
jaeyoungjang2 <lion85488@gmail.com>
2022-06-18 16:54:08 +0900

    docs: readme 작성

---
+++ src/main/java/com/project/ClubMileage/ClubMileageApplication.java
@@ -0,0 +1,13 @@
+package com.project.ClubMileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubMileageApplication.class, args);
+	}
+
+}
