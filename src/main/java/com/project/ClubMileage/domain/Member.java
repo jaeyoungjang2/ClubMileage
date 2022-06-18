851eb47ac68c276b069f7f126f6fc77ad9a59b03
jaeyoungjang2 <lion85488@gmail.com>
2022-06-18 20:59:34 +0900

    feat: 회원 가입 기능 추가

--- src/main/java/com/project/ClubMileage/domain/Member.java
+++ src/main/java/com/project/ClubMileage/domain/Member.java
@@ -1,9 +1,13 @@
 package com.project.ClubMileage.domain;
 
 import com.project.ClubMileage.dto.request.MemberRequestDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -13,9 +17,14 @@ public class Member {
     @GeneratedValue
     private Long id;
 
+    private String uuid = UUID.randomUUID().toString();
+
     private String username;
 
     private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<Review> reviews = new ArrayList<>();
 
     @OneToOne
     private Point point;
