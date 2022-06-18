f87c8bea9854c0ae5aca27fdccfeb2f878eb4640
jaeyoungjang2 <lion85488@gmail.com>
2022-06-18 17:35:10 +0900

    review entity 생성

--- src/main/java/com/project/ClubMileage/domain/Review.java
+++ src/main/java/com/project/ClubMileage/domain/Review.java
@@ -1,2 +1,21 @@
-package com.project.ClubMileage.domain;public class Review {
+package com.project.ClubMileage.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String content;
+
+    private List<Photo> photoes = new ArrayList<>();
+
+    private Place place;
 }
