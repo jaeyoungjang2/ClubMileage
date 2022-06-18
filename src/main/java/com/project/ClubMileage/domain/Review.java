c453c02f08768b60cec8c172f4653fb5e229e1f7
jaeyoungjang2 <lion85488@gmail.com>
2022-06-18 21:00:05 +0900

    feat: 이미지 저장 기능 추가

--- src/main/java/com/project/ClubMileage/domain/Review.java
+++ src/main/java/com/project/ClubMileage/domain/Review.java
@@ -1,11 +1,14 @@
 package com.project.ClubMileage.domain;
 
+import com.project.ClubMileage.dto.PostRequestDto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import lombok.Getter;
@@ -18,13 +21,24 @@ public class Review {
     @GeneratedValue
     private Long id;
 
-    private String uuid = UUID.randomUUID().toString();;
+    private String uuid = UUID.randomUUID().toString();
 
     private String content;
 
     @OneToMany(mappedBy = "review")
-    private List<Photo> photoes = new ArrayList<>();
+    private List<Photo> photos = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @OneToOne
     private Place place;
+
+    public Review(PostRequestDto postRequestDto, Member member, List<Photo> photos, Place place) {
+        this.content = postRequestDto.getContent();
+        this.member = member;
+        this.photos = photos;
+        this.place = place;
+    }
 }
--- src/main/java/com/project/ClubMileage/repository/PhotoRepository.java
+++ src/main/java/com/project/ClubMileage/repository/PhotoRepository.java
@@ -3,6 +3,6 @@ package com.project.ClubMileage.repository;
 import com.project.ClubMileage.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<Photo, Long> {
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
 }
